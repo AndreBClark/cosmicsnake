725b8f00a71e2c75f117476c9554a2e4f16e5c6d
Andre <48167848+AndreBClark@users.noreply.github.com>
2021-09-10 19:49:50 +0000

    add snake avoidance

--- src/logic.js
+++ src/logic.js
@@ -145,6 +145,25 @@ function move(gameState) {
 
     // TODO: Step 3 - Don't collide with others.
     // Use information in gameState to prevent your Battlesnake from colliding with others.
+    const snakes = gameState.board.snakes;
+  snakes.forEach((snake) => {
+    const snakeBody = snake.body;
+
+    snakeBody.forEach((b) => {
+      if (myHead.x === b.x - 1 && myHead.y === b.y) {
+        possibleMoves.right = false;
+      }
+      if (myHead.x === b.x + 1 && myHead.y === b.y) {
+        possibleMoves.left = false;
+      }
+      if (myHead.y === b.y - 1 && myHead.x === b.x) {
+        possibleMoves.up = false;
+      }
+      if (myHead.y === b.y + 1 && myHead.x === b.x) {
+        possibleMoves.down = false;
+      }
+    });
+  });
 
     // TODO: Step 4 - Find food.
     // Use information in gameState to seek out and find food.
